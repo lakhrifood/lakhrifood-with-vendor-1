235817da074546a2da2b1f82e2ef2ebafa0ce76c
Shafin The Developer <sahariar350@gmail.com>
2021-07-03 16:39:07 +0600

    done sign in and index of vendor

--- components/vendor/SellerHeader.js
+++ components/vendor/SellerHeader.js
@@ -4,8 +4,8 @@ import Image from 'next/image';
 function SellerHeader() {
     return (
         <div className="container-fluid">
-            <div className="row p-5">
-                <div className="logo">
+            <div className="row">
+                <div className={`${ styles.logo } p-5`}>
                     <Image src="/./Lakhri food logo.svg" width={200} height={120} alt="" />
                 </div>
             </div>
---
+++ pages/vendor/signin.js
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import Layout from "../../components/Layout"
+import SellerHeader from "../../components/vendor/SellerHeader"
+import styles from "../../styles/signin.module.css";
+
+function signin() {
+    return (
+        <Layout>
+            <SellerHeader></SellerHeader>
+            <div className="container-fluid">
+                <div className="row p-5 flex-column-reverse flex-lg-row">
+                    <div className="col-md-6 ">
+                        <h4 className={`${ styles.welcomeText } mb-5`}>Welcome to <br /> LAKHRI <span className="text-success" >Foods</span> </h4>
+
+                        <form className={`${ styles.signinFrom }`} action="">
+                            <label className="pt-4 pb-2" htmlFor="email">
+                                <h6>Email</h6>
+                            </label>
+                            <input className="form-control" type="email" name="email" id="email" />
+                            <label className="pt-4 pb-2" htmlFor="password">
+                                <h6>Password</h6>
+                            </label>
+                            <input className="form-control" type="password" name="password" id="password" />
+
+                            <div className="form-group text-center">
+                                <input className={`${ styles.submitBtn } btn`} type="submit" value="Submit" />
+                            </div>
+                        </form>
+                    </div>
+                    <div className="col-md-6">
+                        <Image src="/./svg/landing.svg" width={1000} height={500} />
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    )
+}
+
+export default signin
