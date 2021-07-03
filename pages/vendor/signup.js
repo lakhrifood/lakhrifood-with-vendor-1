0ffedabc5e1317865678f0a9a9462258c31f4d94
Anirban Roy <anirban00537@gmail.com>
2021-07-03 15:03:47 +0600

    before break update 😁 😆 😅 😂

--- pages/vendor/signup.js
+++ pages/vendor/signup.js
@@ -1,10 +1,43 @@
 import styles from "../../styles/signup.module.css";
+import Image from "next/image";
+import seling from "../../public/svg/reg celing.svg";
+import treeTub from "../../public/svg/reg tob.svg";
 const signup = () => {
   return (
     <div className={styles.signupContainer}>
-      <div>
+      <div className={styles.header}>
         <div>Lakhrifood logo</div>
-        <div>Vector logo</div>
+        <div className={styles.celingImagehContainer}>
+          <div className={styles.celingSection1}>
+            <Image
+              src={seling}
+              alt="signup lakhri celing"
+              width={250}
+              height={250}
+            />
+          </div>
+          <div className={styles.celingSection2}>
+            <Image
+              src={seling}
+              alt="signup lakhri celing"
+              width={350}
+              height={350}
+            />
+          </div>
+        </div>
+      </div>
+      <div className={styles.formContainer}>
+        <div className={styles.tabSection}>
+          {" "}
+          <Image
+            src={treeTub}
+            alt="signup lakhri celing"
+            width={250}
+            height={250}
+          />
+        </div>
+        <div>form </div>
+        <div>Lady </div>
       </div>
     </div>
   );
