7fe9697c19bde53cd0f47e7657f568d6d0bab786
Anirban Roy <anirban00537@gmail.com>
2021-07-03 14:17:02 +0600

    updates

---
+++ pages/vendor/signup.js
@@ -0,0 +1,13 @@
+import styles from "../../styles/signup.module.css";
+const signup = () => {
+  return (
+    <div className={styles.signupContainer}>
+      <div>
+        <div>Lakhrifood logo</div>
+        <div>Vector logo</div>
+      </div>
+    </div>
+  );
+};
+
+export default signup;
