494f4c358841a58d5ceaf1c8ba911c49d5ccc22d
Shafin The Developer <sahariar350@gmail.com>
2021-07-10 19:48:47 +0600

    manu done

--- components/FoodMenu/Products.js
+++ components/FoodMenu/Products.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './Products.module.css';
+import Image from 'next/image'
 
 function Products() {
     return (
@@ -34,9 +35,222 @@ function Products() {
             </div>
 
             <div className="d-flex flex-wrap justify-content-between pt-3">
-                <div>
-                    
+                <div className={`card ${ styles.productCard }`}>
+                    <Image
+                        src="/./pizza-beer-1200x628-facebook-1200x628.png"
+                        width="100%"
+                        height="280px"
+                        className={`card-img-top ${ styles.cardImage }`}
+                        alt="..." />
+                    <div className="card-body">
+                        <h5>Pizza  Item <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span> (450)</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, illum.</p>
+                        <div className="d-flex justify-content-between align-items-center">
+                            <h3>120.00 BDT</h3>
+                            <button className="btn btn-bg">
+                                <i className="fas fa-cart-plus"></i>
+                            </button>
+                        </div>
+                        <p><i className="far fa-clock me-1"></i> Delivery Time : 35 Min</p>
+                    </div>
                 </div>
+                {/* Another one */}
+                <div className={`card ${ styles.productCard }`}>
+                    <Image
+                        src="/./pizza-beer-1200x628-facebook-1200x628.png"
+                        width="100%"
+                        height="280px"
+                        className={`card-img-top ${ styles.cardImage }`}
+                        alt="..." />
+                    <div className="card-body">
+                        <h5>Pizza  Item <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span> (450)</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, illum.</p>
+                        <div className="d-flex justify-content-between align-items-center">
+                            <h3>120.00 BDT</h3>
+                            <button className="btn btn-bg">
+                                <i className="fas fa-cart-plus"></i>
+                            </button>
+                        </div>
+                        <p><i className="far fa-clock me-1"></i> Delivery Time : 35 Min</p>
+                    </div>
+                </div>
+                {/* Another one */}
+                <div className={`card ${ styles.productCard }`}>
+                    <Image
+                        src="/./pizza-beer-1200x628-facebook-1200x628.png"
+                        width="100%"
+                        height="280px"
+                        className={`card-img-top ${ styles.cardImage }`}
+                        alt="..." />
+                    <div className="card-body">
+                        <h5>Pizza  Item <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span> (450)</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, illum.</p>
+                        <div className="d-flex justify-content-between align-items-center">
+                            <h3>120.00 BDT</h3>
+                            <button className="btn btn-bg">
+                                <i className="fas fa-cart-plus"></i>
+                            </button>
+                        </div>
+                        <p><i className="far fa-clock me-1"></i> Delivery Time : 35 Min</p>
+                    </div>
+                </div>
+                {/* Another one */}
+                <div className={`card ${ styles.productCard }`}>
+                    <Image
+                        src="/./pizza-beer-1200x628-facebook-1200x628.png"
+                        width="100%"
+                        height="280px"
+                        className={`card-img-top ${ styles.cardImage }`}
+                        alt="..." />
+                    <div className="card-body">
+                        <h5>Pizza  Item <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span> (450)</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, illum.</p>
+                        <div className="d-flex justify-content-between align-items-center">
+                            <h3>120.00 BDT</h3>
+                            <button className="btn btn-bg">
+                                <i className="fas fa-cart-plus"></i>
+                            </button>
+                        </div>
+                        <p><i className="far fa-clock me-1"></i> Delivery Time : 35 Min</p>
+                    </div>
+                </div>
+                {/* Another one */}
+                <div className={`card ${ styles.productCard }`}>
+                    <Image
+                        src="/./pizza-beer-1200x628-facebook-1200x628.png"
+                        width="100%"
+                        height="280px"
+                        className={`card-img-top ${ styles.cardImage }`}
+                        alt="..." />
+                    <div className="card-body">
+                        <h5>Pizza  Item <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span> (450)</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, illum.</p>
+                        <div className="d-flex justify-content-between align-items-center">
+                            <h3>120.00 BDT</h3>
+                            <button className="btn btn-bg">
+                                <i className="fas fa-cart-plus"></i>
+                            </button>
+                        </div>
+                        <p><i className="far fa-clock me-1"></i> Delivery Time : 35 Min</p>
+                    </div>
+                </div>
+                {/* Another one */}
+                <div className={`card ${ styles.productCard }`}>
+                    <Image
+                        src="/./pizza-beer-1200x628-facebook-1200x628.png"
+                        width="100%"
+                        height="280px"
+                        className={`card-img-top ${ styles.cardImage }`}
+                        alt="..." />
+                    <div className="card-body">
+                        <h5>Pizza  Item <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span> (450)</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, illum.</p>
+                        <div className="d-flex justify-content-between align-items-center">
+                            <h3>120.00 BDT</h3>
+                            <button className="btn btn-bg">
+                                <i className="fas fa-cart-plus"></i>
+                            </button>
+                        </div>
+                        <p><i className="far fa-clock me-1"></i> Delivery Time : 35 Min</p>
+                    </div>
+                </div>
+                {/* Another one */}
+                <div className={`card ${ styles.productCard }`}>
+                    <Image
+                        src="/./pizza-beer-1200x628-facebook-1200x628.png"
+                        width="100%"
+                        height="280px"
+                        className={`card-img-top ${ styles.cardImage }`}
+                        alt="..." />
+                    <div className="card-body">
+                        <h5>Pizza  Item <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span> (450)</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, illum.</p>
+                        <div className="d-flex justify-content-between align-items-center">
+                            <h3>120.00 BDT</h3>
+                            <button className="btn btn-bg">
+                                <i className="fas fa-cart-plus"></i>
+                            </button>
+                        </div>
+                        <p><i className="far fa-clock me-1"></i> Delivery Time : 35 Min</p>
+                    </div>
+                </div>
+                {/* Another one */}
+                <div className={`card ${ styles.productCard }`}>
+                    <Image
+                        src="/./pizza-beer-1200x628-facebook-1200x628.png"
+                        width="100%"
+                        height="280px"
+                        className={`card-img-top ${ styles.cardImage }`}
+                        alt="..." />
+                    <div className="card-body">
+                        <h5>Pizza  Item <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span> (450)</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, illum.</p>
+                        <div className="d-flex justify-content-between align-items-center">
+                            <h3>120.00 BDT</h3>
+                            <button className="btn btn-bg">
+                                <i className="fas fa-cart-plus"></i>
+                            </button>
+                        </div>
+                        <p><i className="far fa-clock me-1"></i> Delivery Time : 35 Min</p>
+                    </div>
+                </div>
+                {/* Another one */}
+                <div className={`card ${ styles.productCard }`}>
+                    <Image
+                        src="/./pizza-beer-1200x628-facebook-1200x628.png"
+                        width="100%"
+                        height="280px"
+                        className={`card-img-top ${ styles.cardImage }`}
+                        alt="..." />
+                    <div className="card-body">
+                        <h5>Pizza  Item <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span>
+                            <span className={`fa fa-star ${ styles.starIcon } ${ styles.checked }`}></span> (450)</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, illum.</p>
+                        <div className="d-flex justify-content-between align-items-center">
+                            <h3>120.00 BDT</h3>
+                            <button className="btn btn-bg">
+                                <i className="fas fa-cart-plus"></i>
+                            </button>
+                        </div>
+                        <p><i className="far fa-clock me-1"></i> Delivery Time : 35 Min</p>
+                    </div>
+                </div>
+                {/* Another one */}
             </div>
         </div>
     )
