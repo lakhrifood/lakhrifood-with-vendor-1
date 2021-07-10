240a29036359fdb97ec466611123380cc7bc3319
Shafin The Developer <sahariar350@gmail.com>
2021-07-11 01:06:43 +0600

    footer done

--- components/FoodMenu/SideBar.js
+++ components/FoodMenu/SideBar.js
@@ -1,16 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import styles from '../../styles/SideBar.module.css';
+import { makeStyles, Slider } from '@material-ui/core';
+
+
+const useStyles = makeStyles({
+    root: {
+        width: 300,
+    },
+});
+
+function valuetext(value) {
+    return `${ value } BDT`;
+}
 
 function SideBar() {
+    const classes = useStyles();
+    const [value, setValue] = useState([0, 1000]);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
     return (
         <div className={`${ styles.sidebar }`}>
             <ul className={`${ styles.sideMenu } list-unstyled`}>
                 <h1>Menu</h1>
                 <h3>Price </h3>
 
+                <Slider
+                    className={styles.priceSlector}
+                    value={value}
+                    min={0}
+                    max={1000}
+                    onChange={handleChange}
+                    valueLabelDisplay="auto"
+                    aria-labelledby="range-slider"
+                    getAriaValueText={valuetext}
+                />
+                <div className={styles.priceMenu}>
+                    <p>From:</p> <input className="" type="number" value={value[0]} name="" id="from" />
+                    <p>To:</p> <input className="" type="number" value={value[1]} name="" id="from" />
+                </div>
 
-                <h3>Categories</h3>
+                <h3 className="pt-3">Categories</h3>
                 <li className="active">
                     <Link href="/">
                         <a>Home Chefs</a>
--- pages/menu.js
+++ pages/menu.js
@@ -4,6 +4,8 @@ import SideBar from '../components/FoodMenu/SideBar';
 import Navbar from '../components/Navbar'
 import Layout from './../components/Layout';
 import styles from '../styles/Menu.module.css'
+import JoinAsVendorOrRider from '../components/JoinAsVendorOrRider';
+import Footer from '../components/Footer/Footer';
 
 function menu() {
     return (
@@ -13,6 +15,8 @@ function menu() {
                 <SideBar></SideBar>
                 <Products />
             </div>
+            <JoinAsVendorOrRider/>
+            <Footer/>
         </Layout>
     )
 }
