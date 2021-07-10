958beabeeac77220074cc89b03d6e57edf15c2ff
Shafin The Developer <sahariar350@gmail.com>
2021-07-10 17:36:52 +0600

    Navbar done

---
+++ pages/menu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Layout from './../components/Layout';
+
+function menu() {
+    return (
+        <Layout>
+            <Navbar></Navbar>
+        </Layout>
+    )
+}
+
+export default menu
