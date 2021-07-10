be4d83e415a82b97524cc14e2a3d8b5d939e71fa
Shafin The Developer <sahariar350@gmail.com>
2021-07-10 19:11:23 +0600

    done menu almost

---
+++ components/FoodMenu/Products.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import styles from './Products.module.css';
+
+function Products() {
+    return (
+        <div className={styles.productsWrapper}>
+            <div className="d-flex justify-content-between">
+                <div className={`input-group mb-3 ${ styles.searchField }`}>
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder={"Search For Food"}
+                        aria-label="Enter Your Area"
+                        aria-describedby="basic-addon2"
+                    />
+                    <div className="input-group-append">
+                        <button className="btn btn-bg" type="button">
+                            Search
+                        </button>
+                    </div>
+                </div>
+
+                <div className="dropdown">
+                    <a className="btn btn-transparent dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                        Short: By popularity
+                    </a>
+
+                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <li><a className="dropdown-item" href="#">Action</a></li>
+                        <li><a className="dropdown-item" href="#">Another action</a></li>
+                        <li><a className="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="d-flex flex-wrap justify-content-between pt-3">
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Products
--- pages/menu.js
+++ pages/menu.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import Products from '../components/FoodMenu/Products';
+import SideBar from '../components/FoodMenu/SideBar';
 import Navbar from '../components/Navbar'
 import Layout from './../components/Layout';
+import styles from '../styles/Menu.module.css'
 
 function menu() {
     return (
         <Layout>
             <Navbar></Navbar>
+            <div className={`container ${ styles.contentWrapper }`}>
+                <SideBar></SideBar>
+                <Products />
+            </div>
         </Layout>
     )
 }
