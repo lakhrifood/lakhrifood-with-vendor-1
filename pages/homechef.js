4d6feda7d1279e9d963006cae7893011f5fa8b58
Shafin The Developer <sahariar350@gmail.com>
2021-07-11 16:25:37 +0600

    Done

--- pages/homechef.js
+++ pages/homechef.js
@@ -3,12 +3,16 @@ import HomechefCard from '../components/HomechefCard';
 import HomechefDetails from '../components/HomechefDetails';
 import Layout from './../components/Layout';
 import Navbar from './../components/Navbar';
+import JoinAsVendorOrRider from './../components/JoinAsVendorOrRider';
+import Footer from './../components/Footer/Footer';
 
 function homechef() {
     return (
         <Layout>
             <Navbar />
             <HomechefDetails />
+            <JoinAsVendorOrRider />
+            <Footer />
         </Layout>
     )
 }
