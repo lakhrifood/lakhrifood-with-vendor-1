0ea5524d0d51f33d2774e1483a5fcebe509cbee6
Shafin The Developer <sahariar350@gmail.com>
2021-07-12 21:10:00 +0600

    Some style fixed

--- pages/orders.js
+++ pages/orders.js
@@ -8,23 +8,25 @@ function orders() {
     return (
         <Layout>
             <Navbar />
-            <div className="container">
-                <div className="row">
-                    <h1 className={styles.title}>Order History</h1>
-                </div>
-                <div className="d-flex flex-wrap justify-content-around pt-5 pb-5">
-                    <OrderCard />
-                    {/* another one */}
-                    <OrderCard />
-                    {/* another one */}
-                    <OrderCard />
-                    {/* another one */}
-                    <OrderCard />
-                    {/* another one */}
-                    <OrderCard />
-                    {/* another one */}
-                    <OrderCard />
-                    {/* another one */}
+            <div className="page-bg">
+                <div className="container">
+                    <div className="row">
+                        <h1 className={styles.title}>Order History</h1>
+                    </div>
+                    <div className="d-flex flex-wrap justify-content-around pt-5 pb-5">
+                        <OrderCard />
+                        {/* another one */}
+                        <OrderCard />
+                        {/* another one */}
+                        <OrderCard />
+                        {/* another one */}
+                        <OrderCard />
+                        {/* another one */}
+                        <OrderCard />
+                        {/* another one */}
+                        <OrderCard />
+                        {/* another one */}
+                    </div>
                 </div>
             </div>
             <Footer />
--- pages/ordertracking.js
+++ pages/ordertracking.js
@@ -11,21 +11,23 @@ function ordertracking() {
     return (
         <Layout>
             <Navbar />
-            <div className="container">
-                <div className="row pb-5">
-                    <h1 className={styles.title}>Order Tracking</h1>
-                    <h2 className={styles.orderID}>Order ID: asdfasdf</h2>
-                    <OrderTimeline />
-                    {/* <OrderTimelineMobile /> */}
-                    <OrderedProducts />
-                    <h2 className={styles.orderData}>Address: Earth, Milky Way</h2>
-                    <h2 className={styles.orderData}>Payment Method: Bkash</h2>
-                    <h2 className={styles.orderData}>Payment Details: 017855555</h2>
-                    {/* <!-- Button trigger modal --> */}
-                    <button type="button" className={`btn btn-bg mt-5 ${styles.cancelBTN}`} data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                        Cancel Order
-                    </button>
-                    <CancelOrderModal/>
+            <div className="page-bg">
+                <div className="container">
+                    <div className="row pb-5">
+                        <h1 className={styles.title}>Order Tracking</h1>
+                        <h2 className={styles.orderID}>Order ID: asdfasdf</h2>
+                        <OrderTimeline />
+                        {/* <OrderTimelineMobile /> */}
+                        <OrderedProducts />
+                        <h2 className={styles.orderData}>Address: Earth, Milky Way</h2>
+                        <h2 className={styles.orderData}>Payment Method: Bkash</h2>
+                        <h2 className={styles.orderData}>Payment Details: 017855555</h2>
+                        {/* <!-- Button trigger modal --> */}
+                        <button type="button" className={`btn btn-bg mt-5 ${ styles.cancelBTN }`} data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                            Cancel Order
+                        </button>
+                        <CancelOrderModal />
+                    </div>
                 </div>
             </div>
             <Footer />
