70e1612d96ba52d587b234ae653b07f16fb2686e
Shafin The Developer <sahariar350@gmail.com>
2021-07-15 00:16:08 +0600

    DOne

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 // next.config.js
-const withImages = require("next-images");
-module.exports = withImages({
-  webpack(config, options) {
-    return config;
-  },
-});
+// const withImages = require("next-images");
+// module.exports = withImages({
+//   webpack(config, options) {
+//     return config;
+//   },
+// });
 
 module.exports = {
   reactStrictMode: true,
