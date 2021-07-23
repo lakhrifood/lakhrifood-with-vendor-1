72f5ea3556b6ec310fba58b7cb2f1a52711ec144
Anirban Roy <anirban00537@gmail.com>
2021-07-23 17:22:29 +0600

    update

--- pages/vendor/addproducts.js
+++ pages/vendor/addproducts.js
@@ -1,8 +1,63 @@
 const { default: Footer } = require("@/components/vendor/Footer/footer");
 const { default: Navbar } = require("@/components/vendor/Navbar");
 import { Switch } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { FormControl, FormLabel, Flex } from "@material-ui/core";
 const addproducts = () => {
+  const [product, setproduct] = useState({
+    name: "",
+    category: "",
+    imgURL: "",
+    price: 0,
+    totalQuantity: 0,
+    productStatus: "active",
+    description: "",
+    vendorID: "",
+  });
+  const [catagories, setcatagories] = useState([]);
+  const getCatagories = () => {
+    axios
+      .get("http://localhost:5000/catagory")
+      .then((response) => {
+        setcatagories(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const postCloudImage = (image) => {
+    const data = new FormData();
+    data.append("file", image);
+    data.append("upload_preset", "rentahome");
+    data.append("cloud_name", "dfvtfwt6f");
+    fetch("https://api.cloudinary.com/v1_1/dfvtfwt6f/image/upload", {
+      method: "post",
+      body: data,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data, "mycat");
+        setproduct({ ...product, imgURL: data.url });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handleSubmit = () => {
+    console.log(product, "success mycat");
+    axios.post("http://localhost:5000/food/add", product).then(() => {
+      console.log("success mycat");
+    });
+  };
+
+  useEffect(() => {
+    setproduct({ ...product, vendorID: localStorage.getItem("vendorID") });
+    getCatagories();
+  }, []);
+
   return (
     <div className="">
       <Navbar />
@@ -18,63 +73,75 @@ const addproducts = () => {
               class="form-control"
               id="exampleFormControlInput1"
               placeholder="Enter here..."
+              onChange={(e) => {
+                setproduct({ ...product, name: e.target.value });
+              }}
             />
           </div>
           <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">
-              Category
-            </label>
-            <input
-              type="text"
-              class="form-control"
-              id="exampleFormControlInput1"
-              placeholder="Enter here..."
-            />
+            <select
+              class="form-select"
+              aria-label="Default select example"
+              onChange={(e) => {
+                setproduct({ ...product, category: e.target.value });
+              }}
+            >
+              <option selected>Select Category</option>
+              {catagories.map((cat) => (
+                <option value={cat.name}>{cat.name}</option>
+              ))}
+            </select>
           </div>
           <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">
               unit
             </label>
             <input
-              type="text"
+              type="number"
               class="form-control"
               id="exampleFormControlInput1"
               placeholder="Enter here..."
+              onChange={(e) => {
+                setproduct({ ...product, totalQuantity: e.target.value });
+              }}
             />
           </div>
           <div class="mb-3">
             <label for="formFile" class="form-label">
               Photo
             </label>
-            <input class="form-control" type="file" id="formFile" />
+            <input
+              class="form-control"
+              type="file"
+              id="formFile"
+              onChange={(e) => {
+                postCloudImage(e.target.files[0]);
+              }}
+            />
           </div>
           <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">
               Price
             </label>
             <input
-              type="text"
+              type="number"
               class="form-control"
               id="exampleFormControlInput1"
               placeholder="Enter here..."
+              onChange={(e) => {
+                setproduct({ ...product, price: e.target.value });
+              }}
             />
           </div>
-          <div className="row ml-10">
+          {/* <div className="row ml-10">
             <FormControl display="flex" alignItems="center">
               <FormLabel htmlFor="email-alerts" mb="0">
                 Stock Availability
               </FormLabel>
               <Switch id="email-alerts" />
             </FormControl>
-          </div>
-          <div className="row ml-10">
-            <FormControl display="flex" alignItems="center">
-              <FormLabel htmlFor="email-alerts" mb="0">
-                Stock Availability
-              </FormLabel>
-              <Switch id="email-alerts" />
-            </FormControl>
-          </div>
+          </div> */}
+
           <div class="mb-3">
             <label for="exampleFormControlTextarea1" class="form-label">
               Description
@@ -83,10 +150,17 @@ const addproducts = () => {
               class="form-control"
               id="exampleFormControlTextarea1"
               rows="3"
+              onChange={(e) => {
+                setproduct({ ...product, description: e.target.value });
+              }}
             ></textarea>
           </div>
           <div class="col-auto">
-            <button type="submit" class="btn btn-primary mb-3">
+            <button
+              type="submit"
+              class="btn btn-primary mb-3"
+              onClick={handleSubmit}
+            >
               Save
             </button>
           </div>
--- pages/vendor/signin.js
+++ pages/vendor/signin.js
@@ -7,94 +7,100 @@ import { useRouter } from "next/router";
 import { useDispatch } from "react-redux";
 import Link from "next/link";
 const Signin = () => {
-    const dispatch = useDispatch();
-    const [cred, setcred] = useState({
-        email: "",
-        password: "",
-    });
-    const router = useRouter();
-    const [token, settoken] = useState("");
-    const [id, setid] = useState("");
-    const [username, setusername] = useState("");
-    const [email, setemail] = useState("");
-    const [phoneNumber, setphoneNumber] = useState("");
+  const dispatch = useDispatch();
+  const [cred, setcred] = useState({
+    email: "",
+    password: "",
+  });
+  const router = useRouter();
+  const [token, settoken] = useState("");
+  const [id, setid] = useState("");
+  const [username, setusername] = useState("");
+  const [email, setemail] = useState("");
+  const [phoneNumber, setphoneNumber] = useState("");
 
-    const handleSignin = async () => {
-        console.log(cred)
-        try {
-            const { data } = await signinAuthApiForVendor(cred);
-            console.log(data)
-            console.log(data.user._id);
-            settoken(data.token);
-            setid(data.user._id);
-            setphoneNumber(data.phoneNumber);
-            setusername(data.user.ownerName);
-            setemail(data.user.email);
-            dispatch(setAuthTrue());
-            router.push("/vendor/");
-        } catch (error) {
-            console.log(error.message);
-        }
-    };
+  const handleSignin = async () => {
+    console.log(cred);
+    try {
+      const { data } = await signinAuthApiForVendor(cred);
+      console.log(data);
+      console.log(data.user._id);
+      settoken(data.token);
+      setid(data.user._id);
+      setphoneNumber(data.phoneNumber);
+      setusername(data.user.ownerName);
+      setemail(data.user.email);
+      dispatch(setAuthTrue());
+      router.push("/vendor/dashboard");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
-    useEffect(() => {
-        localStorage.setItem("token", token);
-        localStorage.setItem("vendorID", id);
-        localStorage.setItem("userName", username);
-        localStorage.setItem("userEmail", email);
-        localStorage.setItem("phoneNumber", phoneNumber);
-    }, [token, id, username, email, phoneNumber]);
+  useEffect(() => {
+    localStorage.setItem("token", token);
+    localStorage.setItem("vendorID", id);
+    localStorage.setItem("userName", username);
+    localStorage.setItem("userEmail", email);
+    localStorage.setItem("phoneNumber", phoneNumber);
+  }, [token, id, username, email, phoneNumber]);
 
-    return (
-        <div className={styles.container}>
-            <div className={`card ${ styles.cardLog }`}>
-                <div className={styles.header}>
-                    <h1>Welcome !</h1>
-                    <p>Sign Up or Login to Continue.</p>
-                </div>
-                <div className={styles.cardInput}>
-                    <div className="form-group">
-                        <label htmlFor="exampleInputEmail1">Email address</label>
-                        <input
-                            type="email"
-                            className="form-control"
-                            id="exampleInputEmail1"
-                            aria-describedby="emailHelp"
-                            placeholder="Enter email"
-                            value={cred.email}
-                            onChange={(e) => {
-                                setcred({ ...cred, email: e.target.value });
-                            }}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="exampleInputPassword1">Password</label>
-                        <input
-                            type="password"
-                            className="form-control"
-                            id="exampleInputPassword1"
-                            placeholder="Password"
-                            value={cred.password}
-                            onChange={(e) => {
-                                setcred({ ...cred, password: e.target.value });
-                            }}
-                        />
-                    </div>
+  return (
+    <div className={styles.container}>
+      <div className={`card ${styles.cardLog}`}>
+        <div className={styles.header}>
+          <h1>Welcome !</h1>
+          <p>Sign Up or Login to Continue.</p>
+        </div>
+        <div className={styles.cardInput}>
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+              value={cred.email}
+              onChange={(e) => {
+                setcred({ ...cred, email: e.target.value });
+              }}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Password"
+              value={cred.password}
+              onChange={(e) => {
+                setcred({ ...cred, password: e.target.value });
+              }}
+            />
+          </div>
 
-                    <button
-                        type="submit"
-                        className="btn btn-bg-for-homechef"
-                        onClick={() => {
-                            handleSignin();
-                        }}
-                    >
-                        Submit
-                    </button>
-                    <p className="text-center">Dont have an account? <strong className="text-color"> <Link href="/vendor/signup">Sign up</Link></strong> </p>
-                </div>
-            </div>
-        </div >
-    );
+          <button
+            type="submit"
+            className="btn btn-bg-for-homechef"
+            onClick={() => {
+              handleSignin();
+            }}
+          >
+            Submit
+          </button>
+          <p className="text-center">
+            Dont have an account?{" "}
+            <strong className="text-color">
+              {" "}
+              <Link href="/vendor/signup">Sign up</Link>
+            </strong>{" "}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signin;
