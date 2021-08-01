9166f9d78ddfd947b2f5fe2e05c81533a1cf92bc
Shafin The Developer <sahariar350@gmail.com>
2021-08-01 21:00:10 +0600

    Done review

--- components/OrderTracking/OrderedProducts.js
+++ components/OrderTracking/OrderedProducts.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
+import ReviewModal from "./ReviewModal";
 
 function OrderedProducts({ products }) {
-  const total = products && products.productInfo.reduce((acc, curr) => acc + curr.price - curr.discount, 0);
-
+  const total = products && products.productInfo.reduce((acc, curr) => acc + (curr.price * curr.quantity) - curr.discount, 0);
   return (
     <div className="pt-3 pb-3">
-      <table className="table table-striped">
+      <table className="table table-responsive table-striped">
         <thead>
           <tr>
             <th scope="col">Status</th>
@@ -20,15 +20,34 @@ function OrderedProducts({ products }) {
         </thead>
         <tbody>
           {products?.productInfo.map((item, index) => (
-            <tr>
-              <th scope="row">{item.orderStatus}</th>
+            <tr key={index}>
+              {console.log(products, "asdfasdfasdf")}
+              <th scope="row">
+                <div style={{ height: "100px" }} className="d-flex flex-column justify-content-center align-items-center text-uppercase">
+                  {item.orderStatus === "pending" &&
+                    <h1>
+                      <i className="fas fa-exclamation-circle text-success ms-2 me-2"></i>{item.orderStatus}
+                    </h1>}
+                  {item.orderStatus === "accepted" &&
+                    <h1><i className="far fa-check-circle text-success ms-2 me-2"></i>{item.orderStatus}</h1>}
+                  {item.orderStatus === "delivered" &&
+                    <>
+                      <h1>
+                        <i className="fas fa-box-open text-success ms-2 me-2"></i>{item.orderStatus}
+                      </h1>
+                      <button type="button" data-bs-toggle="modal" data-bs-target="#staticBackdrop" className="btn btn-bg mt-2">Give Review</button>
+                      <ReviewModal vendorID={item.vendorID} productID={item.productId._id} orderID={products._id} />
+                    </>
+                  }
+                </div>
+              </th>
               <td>
                 <Image src={item.productId.imgURL} width={100} height={100} />
               </td>
               <td>{item.productId.name} </td>
               <td>{item.price}TK</td>
               <td>{item.discount} TK</td>
-              <td className="text-end">{item.price - item.discount} TK</td>
+              <td className="text-end">{(item.price * item.quantity) - item.discount} TK</td>
             </tr>
           ))}
           <tr>
@@ -46,7 +65,7 @@ function OrderedProducts({ products }) {
           </tr>
         </tbody>
       </table>
-    </div>
+    </div >
   );
 }
 
---
+++ pages/charity.js
@@ -0,0 +1,77 @@
+import Image from 'next/image';
+import Navbar from '../components/Navbar'
+import Footer from '../components/vendor/Footer/Footer';
+import styles from '../styles/Charity.module.css';
+
+function charity() {
+    return (
+        <div>
+            <Navbar />
+            <div className="container text-center">
+                <Image
+                    width={300}
+                    height={300}
+                    src="/./svg/Lakhri food logo.svg"
+                />
+
+                <p className="fs-3">
+                    LAKHRI is an organization aiming to provide home-cooked meals to as many homeless and orphan children as possible for a better hunger-less Bangladesh. We, Lakhri, aim to help them by serving a hot, hygienic, home-cooked meal so that they have the energy and growth to overcome the obstacles in their daily life. A simple gesture can be worth innate humanity. Our intention through Lakhri would be to multiply the effects of goodness through serving a plate of home-cooked food to the hungered ones and that would be that innate humane act.
+                </p>
+
+                <p className="pt-5 fs-3">
+                    "(The righteous are those) who feed the poor, the orphan and the captive for the love of God, saying: 'We feed you for the sake of God Alone; we seek from you neither reward nor thanks.'" - The Holy Quran, 76:8-9
+                </p>
+
+                <p className="pt-5 pb-4 fs-3">
+                    The mission of Lakhri: “To bring change to the lives of homeless and orphan kids”
+                </p>
+            </div>
+            <div>
+                <img
+                    className={styles.charityFristImage}
+                    src="/./WhatsApp Image 2021-07-09 at 8.24.26 PM.png"
+                />
+            </div>
+            <div className="container text-center">
+                <p className="fs-3 pt-5">
+                    ZERO HUNGER, Sustainable Development Goal (SDG) of 2030 is the second goal in the list of developments proposed by Bangladesh, to achieve access to sufficient and nutritious food all year round for all, and Lakhri as an NGO would make it happen as their mission. Lakhri would run through means of charitable purpose. Through collaborated organizations and individual donations, Lakhri plans to help with the ZERO HUNGER SDG of 2030 and will work closely to eliminate homelessness in the country. The entire system of cash flow of Lakhri would be transparent and thus spending on the sole purpose of logistical and raw materials for making homely fresh cooked food. The food that is cooked at home is the main desire of every hungered soul and on a large scale, it will surely generate a huge chain of homely cooked food for the hungered children all over Bangladesh. As per long-term visions, it will spread throughout Bangladesh
+                </p>
+                <p className="fs-3 pt-5 pb-4">
+                    The main vision of Lakhri: ‘To sustain in the field till every hungry soul does not dim out their hope of light while struggling to better their lives on the street.”
+                </p>
+            </div>
+            <div>
+                <img
+                    className={styles.charityFristImage}
+                    src="/./WhatsApp Image 2021-07-09 at 8.24.30 PM.png"
+                />
+            </div>
+            <div className="container text-center">
+                <p className="fs-3 pt-5">
+                    <strong>ZERO HUNGER</strong> the SDGs aim to end all forms of hunger and malnutrition by 2030, making sure all people – especially children – have access to sufficient and nutritious food all year round. This involves promoting sustainable agricultural practices: supporting small-scale farmers and allowing equal access to land, technology, and markets. It also requires international cooperation to ensure investment in infrastructure and technology to improve agricultural productivity. Together with the other goals set out here, we can end hunger by 2030.
+                </p>
+
+                <p className="fs-3 pt-5 pb-4">
+                    Why choose Lakhri for collaboration?
+                </p>
+            </div>
+            <div>
+                <img
+                    className={styles.charityFristImage}
+                    src="/./WhatsApp Image 2021-07-09 at 8.24.33 PM (1).png"
+                />
+            </div>
+            <div className="container text-center pb-5">
+                <p className="fs-3 pt-5">
+                    A business like yours would definitely want to expand market share by promotion, as a socially responsible organization would like to bring you a portion of the market share at your specific business. We carry out CSR to the society and with your support, we can give back to society by this collaboration through a small percentage of donation or charity. The customers we are bringing in through our motive would have an intention to donate to our cause, therefore a small percentage of any order or the designated products order will sponsor these hungry homeless children. As a citizen and an entrepreneur, both CSR and humane act is carried out through this intention.
+                </p>
+                <p className="fs-3 pt-5 pb-5">
+                    With that, we hope that you would be a part of Lakhri’s journey and be the Social Superheroes Bangladesh needs!
+                </p>
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default charity
