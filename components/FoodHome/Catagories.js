c5d9b22e3f0ffce32219f661e4b83edf558f4ec2
Anirban Roy <anirban00537@gmail.com>
2021-08-02 14:58:17 +0600

    update

--- components/FoodHome/Catagories.js
+++ components/FoodHome/Catagories.js
@@ -26,16 +26,13 @@ const responsive = {
   },
 };
 
-
 const Catagories = () => {
   return (
     <div className={styles.cataContainer}>
       <h1>Categories</h1>
 
-
-      <div className={`${ styles.categoryWrapper } container pb-3`}>
-        <div className={`${ styles.cataCon }`}>
-
+      <div className={`${styles.categoryWrapper} container pb-3`}>
+        <div className={`${styles.cataCon}`}>
           <div className={styles.Items}>
             <Link href="/homechefs">
               <div className={styles.item}>
@@ -48,114 +45,113 @@ const Catagories = () => {
                 <h1>Home Chef's</h1>
               </div>
             </Link>
+          </div>
+        </div>
 
-      <div className={styles.cataCon}>
-        <div className={styles.Items}>
-          <div className={styles.item}>
-            <Image
-              src="/../public/chef.png"
-              alt="Picture of the author"
-              width={90}
-              height={110}
-            />
-            <h1>Combo Meals</h1>
+        <div className={styles.cataCon}>
+          <div className={styles.Items}>
+            <div className={styles.item}>
+              <Image
+                src="/../public/chef.png"
+                alt="Picture of the author"
+                width={90}
+                height={110}
+              />
+              <h1>Combo Meals</h1>
+            </div>
+            <div className={styles.Items}>
+              <Link href="/menu?category=Cooked Meals">
+                <div className={styles.item}>
+                  <Image
+                    src="/../public/chef.png"
+                    alt="Picture of the author"
+                    width={90}
+                    height={110}
+                  />
+                  <h1>Cooked Meals</h1>
+                </div>
+              </Link>
+            </div>
+            <div className={styles.Items}>
+              <Link href="/menu?category=Frozen Food Item">
+                <div className={styles.item}>
+                  <Image
+                    src="/../public/chef.png"
+                    alt="Picture of the author"
+                    width={90}
+                    height={110}
+                  />
+                  <h1>Frozen Food Item</h1>
+                </div>
+              </Link>
+            </div>
+            <div className={styles.Items}>
+              <Link href="/menu?category=Desserts Item">
+                <div className={styles.item}>
+                  <Image
+                    src="/../public/chef.png"
+                    alt="Picture of the author"
+                    width={90}
+                    height={110}
+                  />
+                  <h1>Desserts Item</h1>
+                </div>
+              </Link>
+            </div>
 
+            <div className={styles.Items}>
+              <Link href="/menu?category=Organic Product">
+                <div className={styles.item}>
+                  <Image
+                    src="/../public/chef.png"
+                    alt="Picture of the author"
+                    width={90}
+                    height={110}
+                  />
+                  <h1>Organic Product</h1>
+                </div>
+              </Link>
+            </div>
+            <div className={styles.Items}>
+              <Link href="/menu?category=Fast Food Item">
+                <div className={styles.item}>
+                  <Image
+                    src="/../public/chef.png"
+                    alt="Picture of the author"
+                    width={90}
+                    height={110}
+                  />
+                  <h1>Fast Food Item</h1>
+                </div>
+              </Link>
+            </div>
+            <div className={styles.Items}>
+              <Link href="/menu?category=Catering Service">
+                <div className={styles.item}>
+                  <Image
+                    src="/../public/chef.png"
+                    alt="Picture of the author"
+                    width={90}
+                    height={110}
+                  />
+                  <h1>Catering Service</h1>
+                </div>
+              </Link>
+            </div>
+            <div className={styles.Items}>
+              <Link href="/menu?category=Office Meals">
+                <div className={styles.item}>
+                  <Image
+                    src="/../public/chef.png"
+                    alt="Picture of the author"
+                    width={90}
+                    height={110}
+                  />
+                  <h1>Office Meals</h1>
+                </div>
+              </Link>
+            </div>
           </div>
-          <div className={styles.Items}>
-            <Link href="/menu?category=Cooked Meals">
-              <div className={styles.item}>
-                <Image
-                  src="/../public/chef.png"
-                  alt="Picture of the author"
-                  width={90}
-                  height={110}
-                />
-                <h1>Cooked Meals</h1>
-              </div>
-            </Link>
-          </div>
-          <div className={styles.Items}>
-            <Link href="/menu?category=Frozen Food Item">
-              <div className={styles.item}>
-                <Image
-                  src="/../public/chef.png"
-                  alt="Picture of the author"
-                  width={90}
-                  height={110}
-                />
-                <h1>Frozen Food Item</h1>
-              </div>
-            </Link>
-
-          </div>
-          <div className={styles.Items}>
-            <Link href="/menu?category=Desserts Item">
-              <div className={styles.item}>
-                <Image
-                  src="/../public/chef.png"
-                  alt="Picture of the author"
-                  width={90}
-                  height={110}
-                />
-                <h1>Desserts Item</h1>
-              </div>
-            </Link>
-
-          </div>
-
-          <div className={styles.Items}>
-            <Link href="/menu?category=Organic Product">
-              <div className={styles.item}>
-                <Image
-                  src="/../public/chef.png"
-                  alt="Picture of the author"
-                  width={90}
-                  height={110}
-                />
-                <h1>Organic Product</h1>
-              </div>
-            </Link>
-          </div>
-          <div className={styles.Items}>
-            <Link href="/menu?category=Fast Food Item">
-              <div className={styles.item}>
-                <Image
-                  src="/../public/chef.png"
-                  alt="Picture of the author"
-                  width={90}
-                  height={110}
-                />
-                <h1>Fast Food Item</h1>
-              </div>
-            </Link>
-          </div>
-          <div className={styles.Items}>
-            <Link href="/menu?category=Catering Service">
-              <div className={styles.item}>
-                <Image
-                  src="/../public/chef.png"
-                  alt="Picture of the author"
-                  width={90}
-                  height={110}
-                />
-                <h1>Catering Service</h1>
-              </div>
-            </Link>
-          </div>
-          <div className={styles.Items}>
-            <Link href="/menu?category=Office Meals">
-              <div className={styles.item}>
-                <Image
-                  src="/../public/chef.png"
-                  alt="Picture of the author"
-                  width={90}
-                  height={110}
-                />
-                <h1>Office Meals</h1>
-              </div>
-            </Link>
-          </div>
-
         </div>
       </div>
     </div>
