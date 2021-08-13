211e0805f6d3b38f5dd2af9f9124dfd835c143c5
Anirban Roy <anirban00537@gmail.com>
2021-08-13 23:09:47 +0600

    done

--- components/FoodHome/Catagories.js
+++ components/FoodHome/Catagories.js
@@ -2,22 +2,18 @@ import styles from "../../styles/Home.module.css";
 import Image from "next/image";
 import Link from "next/link";
 
-
-
-
 const Catagories = () => {
   return (
     <div className={styles.cataContainer}>
       <h1 className="text-color">Categories</h1>
 
-      <div className={`${ styles.categoryWrapper } container px-5 pb-3`}>
-
+      <div className={`${styles.categoryWrapper} container px-5 pb-3`}>
         <div className={styles.cataCon}>
           <div className={styles.Items}>
             <Link href="/menu?category=Cooked Meals">
               <div className={styles.item}>
                 <Image
-                  src="https://res.cloudinary.com/lakhrifood/image/upload/v1628139276/assests/Group_1490_hcjcrn.svg"
+                  src="https://res.cloudinary.com/lakhrifood/image/upload/v1628139275/assests/turkey_rcgtw1.svg"
                   alt="Picture of the author"
                   width={90}
                   height={110}
@@ -27,10 +23,23 @@ const Catagories = () => {
             </Link>
           </div>
           <div className={styles.Items}>
+            <Link href="/menu?category=Cooked Meals">
+              <div className={styles.item}>
+                <Image
+                  src="https://res.cloudinary.com/lakhrifood/image/upload/v1628139276/assests/Group_1490_hcjcrn.svg"
+                  alt="Picture of the author"
+                  width={90}
+                  height={110}
+                />
+                <h1>Combo Meals</h1>
+              </div>
+            </Link>
+          </div>
+          <div className={styles.Items}>
             <Link href="/menu?category=Frozen Food Item">
               <div className={styles.item}>
                 <Image
-                  src="https://res.cloudinary.com/lakhrifood/image/upload/v1628139275/assests/turkey_rcgtw1.svg"
+                  src="https://res.cloudinary.com/lakhrifood/image/upload/v1628139276/assests/frozen-yogurt_1_skqonn.svg"
                   alt="Picture of the author"
                   width={90}
                   height={110}
@@ -43,7 +52,7 @@ const Catagories = () => {
             <Link href="/menu?category=Desserts Item">
               <div className={styles.item}>
                 <Image
-                  src="https://res.cloudinary.com/lakhrifood/image/upload/v1628139276/assests/frozen-yogurt_1_skqonn.svg"
+                  src="https://res.cloudinary.com/lakhrifood/image/upload/v1628139274/assests/cake_f3igvv.svg"
                   alt="Picture of the author"
                   width={90}
                   height={110}
@@ -57,7 +66,7 @@ const Catagories = () => {
             <Link href="/menu?category=Organic Product">
               <div className={styles.item}>
                 <Image
-                  src="https://res.cloudinary.com/lakhrifood/image/upload/v1628139274/assests/cake_f3igvv.svg"
+                  src="https://res.cloudinary.com/lakhrifood/image/upload/v1628139274/assests/organic_ig7zry.svg"
                   alt="Picture of the author"
                   width={90}
                   height={110}
@@ -70,7 +79,7 @@ const Catagories = () => {
             <Link href="/menu?category=Fast Food Item">
               <div className={styles.item}>
                 <Image
-                  src="https://res.cloudinary.com/lakhrifood/image/upload/v1628139274/assests/organic_ig7zry.svg"
+                  src="https://res.cloudinary.com/lakhrifood/image/upload/v1628139274/assests/Fill_Outline_dh0uzo.svg"
                   alt="Picture of the author"
                   width={90}
                   height={110}
@@ -83,7 +92,7 @@ const Catagories = () => {
             <Link href="/menu?category=Catering Service">
               <div className={styles.item}>
                 <Image
-                  src="https://res.cloudinary.com/lakhrifood/image/upload/v1628139274/assests/Fill_Outline_dh0uzo.svg"
+                  src="https://res.cloudinary.com/lakhrifood/image/upload/v1628139273/assests/waiter_r9l6x3.svg"
                   alt="Picture of the author"
                   width={90}
                   height={110}
@@ -96,7 +105,7 @@ const Catagories = () => {
             <Link href="/menu?category=Office Meals">
               <div className={styles.item}>
                 <Image
-                  src="https://res.cloudinary.com/lakhrifood/image/upload/v1628139273/assests/waiter_r9l6x3.svg"
+                  src="https://res.cloudinary.com/lakhrifood/image/upload/v1628853374/assests/breakfast_wcve0v.svg"
                   alt="Picture of the author"
                   width={90}
                   height={110}
